e67549520cfa1696b246ec7166c1a1bb3835ae6f
Roger Firpo Curcoll <roger.firpo@gmail.com>
2018-02-13 16:30:35 +0100

    add downloading

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from super3.model import Serie, Episodi, Base
-from super3.scrapper import get_series, get_serie_videos
+from super3.scrapper import get_series, get_serie_videos, get_video_download_url
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import urllib
 
 
 def main():
@@ -18,14 +19,22 @@ def main():
     for serie, url in get_series('Kratt'):
         sr = Serie(nom=serie, url=url)
         for episodi, url in get_serie_videos(sr.url):
-            ep = Episodi(serie=sr, nom=episodi, url=url)
+            mp4 = get_video_download_url(url)
+            ep = Episodi(serie=sr, nom=episodi, url=url, mp4=mp4)
         s.add(sr)
     s.commit()
 
     for serie in s.query(Serie).all():
-        print(serie)
-        for episode in serie.episodis:
-            print(episode)
+        print('Sèrie: {}'.format(serie))
+        for episodi in serie.episodis:
+            print('  - Ep: {}'.format(episodi))
+            print('    {}'.format(episodi.url))
+            print('    {}'.format(episodi.mp4))
+            filename = '{}-{}.mp4'.format(
+                serie.nom.lower().replace(' ', '-'),
+                episodi.nom.lower().replace(' ', '-'))
+            urllib.request.urlretrieve(episodi.mp4, filename)
+            print('    Descarregat')
 
 if __name__ == '__main__':
     main()
