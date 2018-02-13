304d1a37504c33c332d41d95acdfe660fe47d7ac
Roger Firpo Curcoll <roger.firpo@gmail.com>
2018-02-13 15:54:11 +0100

    main: iterate through episodes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from super3.model import Serie, Episodi, Base
-from super3.scrapper import get_series
+from super3.scrapper import get_series, get_serie_videos
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -16,13 +16,16 @@ def main():
 
     s = session()
     for serie, url in get_series('Kratt'):
-        sr = Serie(name=serie, url=url)
+        sr = Serie(nom=serie, url=url)
+        for episodi, url in get_serie_videos(sr.url):
+            ep = Episodi(serie=sr, nom=episodi, url=url)
         s.add(sr)
     s.commit()
 
     for serie in s.query(Serie).all():
         print(serie)
-
+        for episode in serie.episodis:
+            print(episode)
 
 if __name__ == '__main__':
     main()
