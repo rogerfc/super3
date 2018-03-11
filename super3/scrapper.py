5d419a6b023d1bfa62af248ed243bf5e54b165d5
Roger Firpo Curcoll <roger.firpo@gmail.com>
2018-03-11 16:26:20 +0100

    scrapper: new url with more series

--- super3/scrapper.py
+++ super3/scrapper.py
@@ -8,7 +8,7 @@ from urllib.parse import urlparse
 import os.path
 
 site = 'http://www.ccma.cat'
-main_url = '/tv3/super3/videos'
+main_url = '/tv3/super3/series-i-programes'
 
 def get_series(filter=None):
     result = requests.get(urljoin(site, main_url))
@@ -16,11 +16,11 @@ def get_series(filter=None):
     soup = BeautifulSoup(content, 'lxml')
 
     for serie in [
-            s.find('p', 'titol').find('a')
-            for s in soup.find_all("div", "F-llistaItem")]:
-        title = serie.string.strip()
+            s.find('a').attrs
+            for s in soup.find_all("div", "F-itemContenidorIntern")]:
+        title = serie['title']
         if not filter or filter.lower() in title.lower():
-            yield title, urljoin(site, serie.attrs['href'])
+            yield title, urljoin(site, serie['href'])
 
 
 def get_serie_episodes(url, filter=None):
